f2f03d7e4a9516d686f09bbd3d864aa9584c9bb5
Entisar <Code.g.light@gmail.com>
2025-05-20 07:18:57 +0300

    re-import  Header to fix reference issues

--- Riyadh-seasons/src/App.jsx
+++ Riyadh-seasons/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Header from './components/header'
+import Header from './components/Header'
 import Footer from './components/Footer'
 import CardGrid from './components/CardGrid'
 import Hero from './components/Hero'
